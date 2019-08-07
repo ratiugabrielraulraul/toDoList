2f3c3541aa98e0ff15665bbaac329a1d8037c7c6
Raul <ratiugabrielraul@gmail.com>
2019-08-07 20:46:02 +0300

    added service and dto(data transfer objects) layers

---
+++ src/main/java/org/fasttrackit/service/ToDoItemService.java
@@ -0,0 +1,32 @@
+package org.fasttrackit.service;
+
+import org.fasttrackit.domain.ToDoItem;
+import org.fasttrackit.persistence.ToDoItemRepository;
+import org.fasttrackit.transfer.SaveToDoItemRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ToDoItemService {
+
+    private ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+
+    public void createToDoItem(SaveToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Creating toDoItem: " + request);
+        //delegating db interactions to repository (single-responsability principle)
+        toDoItemRepository.createToDoItem(request.getDescription(), request.getDeadline());
+
+
+    }
+
+    public List<ToDoItem> getToDoItem() throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Retrieving toDoItems...");
+        return toDoItemRepository.getToDoItems();
+    }
+
+    public  void deleteToDoItem(long id ) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Deleting toDoItem:" +id);
+        toDoItemRepository.deleteToDoItem(id);
+    }
+}
