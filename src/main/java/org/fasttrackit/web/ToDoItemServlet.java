3e62adf8c37e241f66276e1a9794eb968bc35de7
Raul <ratiugabrielraul@gmail.com>
2019-08-14 20:11:45 +0300

    finalized createToDoItem endpoint

--- src/main/java/org/fasttrackit/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/web/ToDoItemServlet.java
@@ -1,16 +1,20 @@
 package org.fasttrackit.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sun.net.httpserver.HttpServer;
 import org.fasttrackit.service.ToDoItemService;
 import org.fasttrackit.transfer.SaveToDoItemRequest;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+
+@WebServlet("/to-do-items")
 
 public class ToDoItemServlet extends HttpServlet {
 
@@ -19,8 +23,10 @@ public class ToDoItemServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
         SaveToDoItemRequest request =
         objectMapper.readValue(req.getReader(), SaveToDoItemRequest.class);
+
 
 
         try {
